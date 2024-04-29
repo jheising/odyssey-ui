eb3748a6b79f93162fb90fc596a0fe53d24c992e
jheising <jheising@gmail.com>
2024-04-28 22:33:48 -0700

    Updates

--- src/primitives/Text.tsx
+++ src/primitives/Text.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Text as RNText } from "react-native";
 import { TextProps } from "react-native/Libraries/Text/Text";
 import { ColorVariant } from "../Theme";
-
-// @ts-ignore
-import Gradient from "react-native-css-gradient";
 import { useTheme } from "../hooks/useTheme";
 
 export const Text = (props: TextProps & {
